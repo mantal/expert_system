2b3e6221ba56cd672dfc2f0022d9766cd8a9ae47
David LANCAR <dlancar@e3r5p11.42.fr>
2016-04-11 15:31:00 +0200

    ADD MOAR THINGS

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,13 @@ mod rule;
 mod parser;
 
 use std::env;
+use std::error::Error;
+use std::io::{self, Read, Write};
 
 use token::Token;
 use token::Operators;
 use rule::Rule;
+
 
 fn get_next(expr: &mut Vec<Token>) -> usize {
 	let mut i = 0;
@@ -41,6 +44,22 @@ fn main() {
         panic!("Usage: expert_system file");
     }
     parser::parse_file(parser::get_file(env::args().nth(1).unwrap()), &mut rules);
+
+    loop {
+        let mut line = String::new();
+
+        print!("?> ");
+        io::stdout().flush().expect("IO error");
+        match io::stdin().read_line(&mut line) {
+            Err(e) => panic!("Could not read stdin: {}", Error::description(&e)),
+            Ok(_) => (),
+        }
+        
+        if line == "exit" {
+            break ;
+        }
+        parser::parse_file(line, &mut rules);
+    }
 }
 //TODO get_file in main
 //TODO TODO TODO rewrite logical op functions so that they dont eval variable unless needed
